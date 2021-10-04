29ccaf4e53f941c69b34ff4384c408a5cdf17abf
Fernando Richter <fgt.fernando@gmail.com>
2021-10-04 15:36:25 -0300

    fix: signup test message

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
-  test('Should  return 400 if no name is provided', () => {
+  test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
