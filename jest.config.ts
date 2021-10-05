8690e11c00d681057da970a5e6cb2fb44217d77e
Fernando Richter <fgt.fernando@gmail.com>
2021-10-05 18:41:13 -0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,17 +4,11 @@
 */
 
 export default {
-  // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-  // The test environment that will be used for testing
   testEnvironment: 'node',
-  // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
